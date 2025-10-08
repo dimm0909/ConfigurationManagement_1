594b1b39ae6b79d9e94e3614968cb3ac075d0302
Dmitriy Chizhikov <dimm0909@gmail.com>
2025-10-08 12:56:29 +0300

    Some fixes and optimization

--- main.py
+++ main.py
@@ -1,31 +1,29 @@
+# Вариант №29
+
 import os
 import socket
 
-# Вариант №29
 
+def execute(instruction):
+    if str(instruction)[0] != '$':
+        parts = str(instruction).split()
+        if not parts:
+            return
 
+        cmd = parts[0]
+        args = parts[1:]
 
+        if cmd == "exit":
+            exit()
+        elif cmd == "ls":
+            print(f"ls {' '.join(args)}")
+        elif cmd == "cd":
+            print(f"cd {' '.join(args)}")
+        else:
+            print(f"Error: unknown command '{cmd}'")
+    else:
+        print(os.getenv(instruction[1:]))
 
-def main():
-    username = os.getlogin()
-    machineId = socket.gethostname()
-    while True:
-        try:
-            command, *args = input(f"{username}@{machineId}:~$ ").split(" ")
-
-            if command == "exit":
-                break
-            elif command == "ls":
-                print(command + " " + " ".join(args))
-            elif command == "cd":
-                print(command + " " + " ".join(args))
-            elif command.find("$") != -1:
-                print(os.environ[str.replace(command, "$", "")])
-            else:
-                print("Unknown command: " + command)
-        except Exception as err:
-            print("Error: " + str(err))
-
-
-if __name__ == '__main__':
-    main()
+while True:
+    command = input(f"{os.getlogin()}@{socket.gethostname()}:~$ ")
+    execute(command)
